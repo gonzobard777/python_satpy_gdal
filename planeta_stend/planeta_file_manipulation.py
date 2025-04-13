d87cee9e6670b21db04eab3340f0fc493994e349
gonzobard777 <gonzo.bard@dwfe.ru>
2025-04-14 00:43:55 +0300

    .

--- planeta_stend/planeta_file_manipulation.py
+++ planeta_stend/planeta_file_manipulation.py
@@ -1,15 +1,32 @@
+import shutil
 from datetime import datetime, timedelta
 from util.my_fs import *
 
-target_dir='D:/_SAT/PLANETA_STEND/a1_ch04'
-pattern='a1_*_ch04.tif'
+dtime=datetime.fromisoformat('2025-04-14T00:00:00.000Z')-timedelta(hours=5)
+base_dir_path='D:/_SAT/PLANETA_STEND'
+mock_dir_path=f'{base_dir_path}/mock_data' # папка с результирующими папками/файлами
+metadata=[
+    ['a1','ch04',f'{base_dir_path}/A'],
+    ['a2','ch04',f'{base_dir_path}/A'],
+    ['e2','ch04',f'{base_dir_path}/E'],
+    ['e3','ch04',f'{base_dir_path}/E'],
+    ['e4','ch04',f'{base_dir_path}/E'],
+]
 
-copy_files('D:/_SAT/PLANETA_STEND/A',target_dir,pattern,)
-# rename_files(target_dir,pattern,'20250401','20250413')
+shutil.rmtree(mock_dir_path)
+for sat,channel,src_dir_path in metadata:
+    target_dir=f'{mock_dir_path}/{sat}_{channel}'
+    pattern = f'{sat}_*_{channel}.tif'
+    copy_files(src_dir_path,target_dir,pattern)
+    # rename_files(target_dir,pattern,'20250401','20250413')
+    correct_dates(
+        target_dir,
+        dtime,                # начать с этого времени
+        timedelta(minutes=15) # потом прибавлять этот шаг
+    )
 
-correct_dates(
-    target_dir,
-    datetime.fromisoformat('2025-04-13T21:00:00.000Z')-timedelta(hours=5),
-    timedelta(minutes=15)
-)
+
+
+
+
 
