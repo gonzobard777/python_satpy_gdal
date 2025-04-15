e8e4fc80e118ff8e498689c259e66a75c7b11e2f
gonzobard777 <gonzo.bard@dwfe.ru>
2025-04-15 06:12:33 +0300

    .

--- adjust_geotif/run_adjust_geotif.py
+++ adjust_geotif/run_adjust_geotif.py
@@ -9,11 +9,10 @@ input_file_pattern=f"{C.ASSET_DIR}/{prefix}_{timestamp}*_{channel}.tif"
 
 output_file_path= f"{C.ASSET_DIR}/result/{prefix}-{channel}-{timestamp}"
 output_file=f"{output_file_path}.tif"
-tmp_output_file = f"{output_file_path}_temp.tif"
 
 adjust_geotif(
     input_file_pattern,
     output_file,
-    tmp_output_file,
+    invert=True,
 )
 
