110443850f2a39158531cc020a99a5e76c24e50f
gonzobard777 <gonzo.bard@dwfe.ru>
2025-08-20 23:16:38 +0300

    .

--- south_up_2_north_up/run_south_up_2_north_up.py
+++ south_up_2_north_up/run_south_up_2_north_up.py
@@ -3,6 +3,6 @@ from south_up_2_north_up import south_up_2_north_up
 
 filename="MSG3_VIS006-20250401_1200.tif"
 input_file=f"{C.ASSET_DIR}/{filename}"
-output_file=f"{C.ASSET_DIR}/delete_me/MSG3_VIS006-20250401_1200.tif"
+output_file=f"{C.ASSET_DIR}/result/MSG3_VIS006-20250401_1200.tif"
 
 south_up_2_north_up(input_file,output_file)
